1a7f166d1fbb6c01869e6208b290d23340863813
Jan Runo <jan.runo@whu.edu>
2018-04-05 14:38:37 +0200

    adding benefits to the scaper

--- index.js
+++ index.js
@@ -22,7 +22,26 @@ let kununuBaseprint = [
   'Umgang mit älteren Kollegen',
   'Karriere / Weiterbildung',
   'Gehalt / Sozialleistungen',
-  'Image'
+  'Image',
+  'workhours',
+  'homeoffice',
+  'cafeteria',
+  'food',
+  'childcare',
+  'pension',
+  'accessibility',
+  'fitness',
+  'medic',
+  'coaching',
+  'parking',
+  'transportation',
+  'discount',
+  'car',
+  'phone',
+  'shares',
+  'events',
+  'internet',
+  'dogs'
 ]
 
 
@@ -32,7 +51,7 @@ const scrapeKununuPage = (rawCompanyName) => {
       if(!error && typeof body !== 'undefined'){
         let $ = cheerio.load(body);
 
-        let excelRow = ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-']
+        let excelRow = ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-']
         let companyName = $('.company-name').text().trim()
         let industry = $('.company-profile-sub-title').children().last().text().trim()
         let totalScore = $('.review-rating-value').text().trim()
@@ -45,6 +64,48 @@ const scrapeKununuPage = (rawCompanyName) => {
         excelRow[3] = typeof recommendationScore !== 'undefined' ? recommendationScore : '-'
         excelRow[4] = typeof numberOfReviews !== 'undefined' ? numberOfReviews : '-'
 
+        //console.log(body)
+        let workhours = typeof $('.benefit-workhours').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-workhours').text().trim().split(' bei ')[1]).split(' ')[0]
+        let homeoffice = typeof $('.benefit-homeoffice').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-homeoffice').text().trim().split(' bei ')[1]).split(' ')[0]
+        let cafeteria = typeof $('.benefit-cafeteria').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-cafeteria').text().trim().split(' bei ')[1]).split(' ')[0]
+        let food = typeof $('.benefit-food').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-food').text().trim().split(' bei ')[1]).split(' ')[0]
+        let childcare = typeof $('.benefit-childcare').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-childcare').text().trim().split(' bei ')[1]).split(' ')[0]
+        let pension = typeof $('.benefit-401k').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-401k').text().trim().split(' bei ')[1]).split(' ')[0]
+        let accessibility = typeof $('.benefit-accessibility').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-accessibility').text().trim().split(' bei ')[1]).split(' ')[0]
+        let fitness = typeof $('.benefit-fitness').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-fitness').text().trim().split(' bei ')[1]).split(' ')[0]
+        let medic = typeof $('.benefit-medic').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-medic').text().trim().split(' bei ')[1]).split(' ')[0]
+        let coaching = typeof $('.benefit-coaching').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-coaching').text().trim().split(' bei ')[1]).split(' ')[0]
+        let parking = typeof $('.benefit-parking').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-parking').text().trim().split(' bei ')[1]).split(' ')[0]
+        let transportation = typeof $('.benefit-transportation').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-transportation').text().trim().split(' bei ')[1]).split(' ')[0]
+        let discount = typeof $('.benefit-discount').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-discount').text().trim().split(' bei ')[1]).split(' ')[0]
+        let car = typeof $('.benefit-car').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-car').text().trim().split(' bei ')[1]).split(' ')[0]
+        let phone = typeof $('.benefit-phone').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-phone').text().trim().split(' bei ')[1]).split(' ')[0]
+        let shares = typeof $('.benefit-shares').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-shares').text().trim().split(' bei ')[1]).split(' ')[0]
+        let events = typeof $('.benefit-events').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-events').text().trim().split(' bei ')[1]).split(' ')[0]
+        let internet = typeof $('.benefit-internet').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-internet').text().trim().split(' bei ')[1]).split(' ')[0]
+        let dogs = typeof $('.benefit-dogs').text().trim().split(' bei ')[1] === 'undefined' ? 0 : ($('.benefit-dogs').text().trim().split(' bei ')[1]).split(' ')[0]
+
+        excelRow[18] = workhours
+        excelRow[19] = homeoffice
+        excelRow[20] = cafeteria
+        excelRow[21] = food
+        excelRow[22] = childcare
+        excelRow[23] = pension
+        excelRow[24] = accessibility
+        excelRow[25] = fitness
+        excelRow[26] = medic
+        excelRow[27] = coaching
+        excelRow[28] = parking
+        excelRow[29] = transportation
+        excelRow[30] = discount
+        excelRow[31] = car
+        excelRow[32] = phone
+        excelRow[33] = shares
+        excelRow[34] = events
+        excelRow[35] = internet
+        excelRow[36] = dogs
+
+        excelRow[4] = typeof numberOfReviews !== 'undefined' ? numberOfReviews : '-'
 
         $('.company-profile-rating-group').find('.rating-group').each(function( index ) {
           let ratingTitle = $(this).find('.rating-title').text().trim()
@@ -68,10 +129,10 @@ fs.writeFile('./excelFile.csv', kununuBaseprint + '\n', function(err) {
   if(err) {
     return console.log(err)
   }
-    console.log("Excel file created!")
+  console.log("Excel file created!")
 })
 
-let rawCompanyNames = ['movinga', 'trumpf', 'bmw']
+let rawCompanyNames = ['movinga']
 
 for(let rawCompanyName in rawCompanyNames){
   let currentCompanyName = rawCompanyNames[rawCompanyName]
